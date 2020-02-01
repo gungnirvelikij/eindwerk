549f49bed09f686fd158586da3c81c2ac29dd0dd
gungnir <griever_lionheart89@hotmail.com>
2020-02-01 11:24:05 +0100

    fixed compile.bat to include object linking

--- 328p/compile.bat
+++ 328p/compile.bat
@@ -1,3 +1,10 @@
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o main.o main.c
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o serial.o serial.c
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o adc.o adc.c
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o pwm.o pwm.c
+
+avr-gcc -mmcu=atmega328p main.o serial.o adc.o pwm.o -o evse
+
 echo creating hex file
 avr-objcopy -O ihex -R .eeprom evse evse.hex
 echo uploading
