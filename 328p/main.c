e9249b72a084a73b7cf28e3afd00d9d3d7ec1c3a
gungnir <griever_lionheart89@hotmail.com>
2020-02-12 11:50:58 +0100

    added header file for main

--- 328p/main.c
+++ 328p/main.c
@@ -1,36 +1,26 @@
+#include "main.h"
 
-#include "pwm.h"
-#include "adc.h"
-#include "serial.h"
-#include "util/delay.h"
-//#define LOOP_UNTIL_CLEARED(sfr, bit) while(sfr & (1 << bit)) {}
-#define ADC_MAX 1023
+const uint8_t delay_ADC = 90; // in µs
 
-
-unsigned short adc_resultaat;
-unsigned short newvalue;
-unsigned char adc_tochar;
-unsigned char adc_toled;
-unsigned char string[255];
+unsigned char ADC_result_string[255];
 unsigned char dutycycle = 10;
-const uint8_t delay_ADC = 90; //in µs
+
 unsigned char charging = 0;
 unsigned char ventilation = 0;
-void ADC_to_serial();
-void check_state(int);
 
 unsigned short ADC_12V;
 unsigned short ADC_9V;
 unsigned short ADC_6V;
 unsigned short ADC_3V;
 unsigned short ADC_0V;
+unsigned short ADC_result;
 
 // ISR(PCINT1_vect){
 // 	ADCSRA |= (1 << ADSC); // start ADC-conversie
 // }
 
 ISR(ADC_vect){
-	adc_resultaat = ADC;
+	ADC_result = ADC;
 	ADC_to_serial();
 }
 
@@ -40,10 +30,10 @@ ISR(TIMER1_COMPA_vect){ // triggered when PWM has reached top -> trigger ADC
 }
 
 void ADC_to_serial(){
-	itoa(adc_resultaat, string, 10);
-	//serial_transmit(string);
+	itoa(ADC_result, ADC_result_string, 10);
+	//serial_transmit(ADC_result_string);
 	//serial_transmit("\n\r");
-	check_state(adc_resultaat);
+	check_state(ADC_result);
 }
 
 void start_charging(){
@@ -59,7 +49,7 @@ void stop_charging(){
 }
 
 void check_state(int reading){ // check reading by ADC (10 bits: 0 to 1024)
-	// 12V (above 11/12 of ADC_MAX)-> no vehicle detected 
+	// 12V-> no vehicle detected 
 	// 9V -> Vehicle detected, no charging required
 	// 6V -> charging required
 	// 3V -> charging only if in ventilated space (outside) 
@@ -89,7 +79,7 @@ void check_state(int reading){ // check reading by ADC (10 bits: 0 to 1024)
 }
 
 void set_ADC_values(){  // set ADC values at start to prevent recalculation at every check needed
-	ADC_12V = ADC_MAX * (11.0/12.0);
+	ADC_12V = ADC_MAX * (11.0/12.0); // set threshold 1V lower to allow discrepancies
        	ADC_9V = ADC_MAX * (8.0/12.0);
 	ADC_6V = ADC_MAX * (5.0/12.0);
 	ADC_3V = ADC_MAX * (2.0/12.0);
@@ -97,31 +87,14 @@ void set_ADC_values(){  // set ADC values at start to prevent recalculation at e
 
 int main(void) {
 	set_ADC_values();
-
 	init_interrupts();
 	adc_init();
-	//serial_begin(9600);
-	//send_string("SERIAL INIT");
 	serial_enable_interrupt();
 	serial_init();
 	pwm_init();
-	//send_string("PWM INIT");
 	set_duty(dutycycle);
-	//serial_write(dutycycle);
-	unsigned short newvalue;
 	while(1)	
 	{
-		
-		//if ((adc_resultaat != newvalue) && (adc_resultaat > 400)){
-		// ADCSRA |= (1 << ADSC);	
-		newvalue = adc_resultaat;
-			adc_tochar = (adc_resultaat / 4);
-			//itoa(adc_resultaat, string, 10);
-			//send_string(string);
-			//send_string("\n");
-			//serial_transmit("test");
-			//serial_transmit("\n\r");
-			//serial_transmit("\n");
-		//}
+		//
 	}
 }
---
+++ 328p/main.h
@@ -0,0 +1,13 @@
+#include "pwm.h"
+#include "adc.h"
+#include "serial.h"
+#include "util/delay.h"
+
+#define ADC_MAX 1023
+
+void ADC_to_serial();
+void start_charging();
+void stop_charging();
+void check_state(int);
+void set_ADC_values();
+
