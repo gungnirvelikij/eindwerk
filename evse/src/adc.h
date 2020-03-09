7e82fa6c122c51a80dda96b95c5025b683fd2d00
goossensbas <goossensbas@gmail.com>
2020-03-09 20:46:04 +0100

    file tree change + Serial in main.c

---
+++ evse/src/adc.h
@@ -0,0 +1,13 @@
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LOOP_UNTIL_CLEARED(sfr, bit) while(sfr & (1 << bit)) {}
+
+void init_interrupts(void);
+void adc_init(void);
+int analogread(void);
---
+++ evse/src/compile_flash.sh
@@ -0,0 +1,14 @@
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o obj/main.o main.c
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o obj/serial.o serial.c
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o obj/adc.o adc.c
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o obj/pwm.o pwm.c
+
+avr-gcc -mmcu=atmega328p obj/main.o obj/serial.o obj/adc.o obj/pwm.o -o evse
+
+
+echo "Creating hex-file"
+avr-objcopy -O ihex -R .eeprom evse evse.hex
+
+echo "Flashing"
+avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:evse.hex
+
