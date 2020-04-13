4773aa562a57738de1da1c3fc2bb2bb01db29134
gungnir <griever_lionheart89@hotmail.com>
2020-04-13 20:00:41 +0200

    added lcd, removed serial debug, changed debugpin to pin for relay, added function for write to lcd to only write when state has changed

--- platformio/Eindwerk-EVSE/src/main.cpp
+++ platformio/Eindwerk-EVSE/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "EVSE.h"
+#include <LiquidCrystal_I2C.h>
 
 //
 // Variables
@@ -8,10 +9,15 @@
 const uint8_t delay_ADC = 0; // in µs
 
 char ADC_result_string[255];
-unsigned char dutycycle = 10;
+unsigned char dutycycle = 0;
 
 unsigned char charging = 0;
 unsigned char ventilation = 0;
+
+unsigned char state = 0;  // 0: not connected, 1: connected no charge, 2: connected charge, 3: connected ventilation no charge, 4: connected ventilation charge, 5: error
+unsigned char current = 0;
+
+LiquidCrystal_I2C lcd(0x3F,20,4);
 
 int reading;
 
@@ -20,7 +26,10 @@ int reading;
 int adcpin = A0;   // analog pin 0
 int pwmpin = 6; // digital pin 6
 int interruptpin = 2; // digital pin 2
-int debugpin = 9;
+int relaypin = 9;
+//SCL = A5
+//SDA = A4
+//VDD led = 5V
 
 //
 // Interrupts
@@ -38,13 +47,13 @@ int debugpin = 9;
 
 void start_charging(){
 	if(!charging){	
-		Serial.println("START \n\r");
+		digitalWrite(relaypin, HIGH);
 	}
 }
 
 void stop_charging(){
 	if(charging){
-		Serial.println("STOP \n\r");
+		digitalWrite(relaypin, LOW);
 	}
 }
 
@@ -57,46 +66,105 @@ void check_state(){ // check reading by ADC (10 bits: 0 to 1024)
   // value -1 to allow for small discrepancy
 
 	if(reading > 915){  // 12V
-		Serial.println("not connected \n\r");
+		//Serial.println("not connected \n\r");
 		stop_charging();
+		set_state(0);
 	} else if(reading > 800) { // 9V
-		Serial.println("Connected, not charging \n\r");
+		//Serial.println("Connected, not charging \n\r");
 		stop_charging();
+		set_state(1);
 	} else if(reading > 700) { // 6V
-		Serial.println("Connected, charging requested\n\r");
+		//Serial.println("Connected, charging requested\n\r");
 		start_charging();
+		set_state(2);
 	} else if(reading > 600) { // 3V
-		Serial.println("Connected, charging with vent requested \n\r");
+		//Serial.println("Connected, charging with vent requested \n\r");
 		
 		if(ventilation){
 			start_charging();
+			set_state(4);
 		} else {
 			stop_charging();
+			set_state(3);
 		}
 	} else { //0V
-		Serial.println("Something's gone wrong\n\r");
+		//Serial.println("Something's gone wrong\n\r");
 		stop_charging();
+		set_state(5);
+	}
+}
+
+void set_state(char newstate){
+	if(state != newstate){
+		state = newstate;
+		write_lcd();
 	}
 }
 
 void set_reading(){
-  digitalWrite(debugpin, HIGH);
   reading = analogRead(adcpin);
-  digitalWrite(debugpin, LOW);
+}
+
+void write_lcd(){
+String state_string = "";
+  switch(state){
+	case 0:
+		state_string = "Not connected";
+	  	break;
+	case 1:
+		state_string = "Not charging";
+	  	break;
+	case 2:
+		state_string = "Charging";
+	  	break;
+	case 3:
+		state_string = "HOT - not charging";
+	  	break;
+	case 4:
+		state_string = "HOT - charging";
+	  	break;
+	default:
+		state_string = "ERROR";
+  }
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print(state_string);
+  lcd.setCursor(1,1);
+  lcd.print(current);
+}
+
+void set_current(char amps){  //max 20A 
+	if(amps < 21){
+		current = amps;
+		dutycycle = amps/0.6;
+	} else {
+		set_current(6);
+	}
 }
 
 void setup() {
-  // put your setup code here, to run once:
+	set_current(6);
+  
+  //serial monitor
   Serial.begin(9600);
-  pinMode(pwmpin, OUTPUT);
-  pinMode(debugpin, OUTPUT);
+  
+  pinMode(relaypin, OUTPUT);
+
+  // interrupt
   pinMode(interruptpin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptpin), set_reading, FALLING);
+  
+  //PWM
+  pinMode(pwmpin, OUTPUT);
   analogWrite(pwmpin, map(100-dutycycle, 0, 100, 0, 255));  // set PWM on basis of dutycycle variable (mapped from % to byte)
+
+	//LCD
+  lcd.init();
+  lcd.backlight();
+  write_lcd();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- 
   check_state(); 
 }
