04ae1bb1dbf75a7581d0574f932986035678026a
goossensbas <goossensbas@gmail.com>
2020-04-13 20:40:00 +0200

    relay working, changed if statement to case with mapping

--- platformio/Eindwerk-EVSE/src/main.cpp
+++ platformio/Eindwerk-EVSE/src/main.cpp
@@ -26,7 +26,7 @@ int reading;
 int adcpin = A0;   // analog pin 0
 int pwmpin = 6; // digital pin 6
 int interruptpin = 2; // digital pin 2
-int relaypin = 9;
+int relaypin = 13;
 //SCL = A5
 //SDA = A4
 //VDD led = 5V
@@ -46,54 +46,65 @@ int relaypin = 9;
 //
 
 void start_charging(){
-	if(!charging){	
+	if(!charging){
 		digitalWrite(relaypin, HIGH);
+		charging = 1;
 	}
 }
 
 void stop_charging(){
 	if(charging){
 		digitalWrite(relaypin, LOW);
+		charging = 0;
 	}
 }
 
 void check_state(){ // check reading by ADC (10 bits: 0 to 1024)
-	// 12V-> no vehicle detected 
+	// 12V-> no vehicle detected
 	// 9V -> Vehicle detected, no charging required
 	// 6V -> charging required
-	// 3V -> charging only if in ventilated space (outside) 
+	// 3V -> charging only if in ventilated space (outside)
 	// 0V -> error
-  // value -1 to allow for small discrepancy
 
-	if(reading > 915){  // 12V
-		//Serial.println("not connected \n\r");
-		stop_charging();
-		set_state(0);
-	} else if(reading > 800) { // 9V
-		//Serial.println("Connected, not charging \n\r");
-		stop_charging();
-		set_state(1);
-	} else if(reading > 700) { // 6V
-		//Serial.println("Connected, charging requested\n\r");
-		start_charging();
-		set_state(2);
-	} else if(reading > 600) { // 3V
-		//Serial.println("Connected, charging with vent requested \n\r");
-		
-		if(ventilation){
-			start_charging();
-			set_state(4);
-		} else {
-			stop_charging();
-			set_state(3);
-		}
-	} else { //0V
-		//Serial.println("Something's gone wrong\n\r");
-		stop_charging();
-		set_state(5);
-	}
+	// map reading from 500-900 to 0-4)
+	int range = map(reading, 500, 900, 0, 4);
+
+	  // do something different depending on the range value:
+	  switch (range) {
+	    case 0:    // reading > 500
+					stop_charging();
+					set_state(5);
+	      break;
+	    case 1:    // reading > 600
+			//Serial.println("Connected, charging with vent requested \n\r");
+
+					if(ventilation){
+							start_charging();
+							set_state(4);
+					}
+					else {
+						stop_charging();
+						set_state(3);
+					}
+	      break;
+	    case 2:    // reading > 700
+					//Serial.println("Connected, charging requested\n\r");
+					start_charging();
+					set_state(2);
+	      break;
+	    case 3:    // reading > 800
+			//Serial.println("Connected, not charging \n\r");
+					stop_charging();
+					set_state(1);
+	      break;
+			case 4:		// reading > 900
+					//Serial.println("not connected \n\r");
+					stop_charging();
+					set_state(0);
+				break;
+
+			}
 }
-
 void set_state(char newstate){
 	if(state != newstate){
 		state = newstate;
@@ -133,7 +144,7 @@ String state_string = "";
   lcd.print(current);
 }
 
-void set_current(char amps){  //max 20A 
+void set_current(char amps){  //max 20A
 	if(amps < 21){
 		current = amps;
 		dutycycle = amps/0.6;
@@ -144,16 +155,16 @@ void set_current(char amps){  //max 20A
 
 void setup() {
 	set_current(6);
-  
+
   //serial monitor
   Serial.begin(9600);
-  
+
   pinMode(relaypin, OUTPUT);
 
   // interrupt
   pinMode(interruptpin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptpin), set_reading, FALLING);
-  
+
   //PWM
   pinMode(pwmpin, OUTPUT);
   analogWrite(pwmpin, map(100-dutycycle, 0, 100, 0, 255));  // set PWM on basis of dutycycle variable (mapped from % to byte)
@@ -166,5 +177,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  check_state(); 
+  check_state();
 }
